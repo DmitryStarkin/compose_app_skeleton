5452cb8023e66e84f818c085b47ffe07110f4e7b
Dmitry <t0506803080@gmail.com>
2025-03-11 13:28:29 +0200

    add implementation of lifecycleOwners

---
+++ compose_app_skeleton/src/main/java/com/starsoft/skeleton/compose/navigation/LifeCicleComposeScreen.kt
@@ -0,0 +1,77 @@
+package com.starsoft.skeleton.compose.navigation
+
+import androidx.annotation.CallSuper
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+
+
+/**
+ * Created by Dmitry Starkin on 11.03.2025 12:24.
+ */
+abstract class LifeCycleComposeScreen: Router.ComposeScreen, LifecycleOwner {
+    private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
+    override val lifecycle: Lifecycle get() = lifecycleRegistry
+    
+    init {
+        lifecycleRegistry.currentState = Lifecycle.State.INITIALIZED
+    }
+    @CallSuper
+    override fun onCreate(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    }
+    @CallSuper
+    override fun onStop(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+    }
+    @CallSuper
+    override fun onStart(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+    }
+    @CallSuper
+    override fun onResume(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    }
+    @CallSuper
+    override fun onPause(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    }
+    @CallSuper
+    override fun onDestroy(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    }
+}
+
+abstract class LifeCycleComposeDialog:  Router.ComposeDialog, LifecycleOwner{
+    
+    private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
+    override val lifecycle: Lifecycle get() = lifecycleRegistry
+    
+    init {
+        lifecycleRegistry.currentState = Lifecycle.State.INITIALIZED
+    }
+    @CallSuper
+    override fun onCreate(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    }
+    @CallSuper
+    override fun onStop(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+    }
+    @CallSuper
+    override fun onStart(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+    }
+    @CallSuper
+    override fun onResume(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    }
+    @CallSuper
+    override fun onPause(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    }
+    @CallSuper
+    override fun onDestroy(owner: LifecycleOwner) {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    }
+}
